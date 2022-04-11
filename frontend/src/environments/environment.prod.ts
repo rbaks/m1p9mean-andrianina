d45fa86d0c425aed3627b8ef9d79ae93dfcf0a68
rbaks <andri.bakoson@gmail.com>
2022-04-11 19:10:15 +0300

    Delete previous project

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { NgxLoggerLevel } from "ngx-logger";
-
-export const environment = {
-  production: true,
-  logLevel: NgxLoggerLevel.OFF,
-  serverLogLevel: NgxLoggerLevel.ERROR,
-  // apiUrl: "http://localhost:3001/api",
-  apiUrl: "https://ekaly-backend.herokuapp.com/api",
-};
